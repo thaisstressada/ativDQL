97a8f47f83f8af07315018ad36e58598ba7b5eb2
thaisstressada <saless.costhais@gmail.com>
2024-08-19 21:54:48 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE jogos_esportivos(
+  esporte varchar(190),
+  equipe_casa varchar(180),
+  equipe_visitante varchar(170),
+  pontuacao_casa DECIMAL(10,2),
+  pontuacao_visitante INT(10),
+  data_jogo DATE
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
